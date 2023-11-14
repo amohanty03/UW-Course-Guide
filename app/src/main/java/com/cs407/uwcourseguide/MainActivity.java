16bf95d68885bb989cb37802426c0de184531878
amohanty03 <mohanty5@wisc.edu>
2023-11-13 21:14:57 -0600

    Added 4 fragments and navbar

--- app/src/main/java/com/cs407/uwcourseguide/MainActivity.java
+++ app/src/main/java/com/cs407/uwcourseguide/MainActivity.java
@@ -1,14 +1,65 @@
 package com.cs407.uwcourseguide;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView
+        .OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView
+                = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView
+                .setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+
+    HomePage firstFragment = new HomePage();
+    SchedulePage secondFragment = new SchedulePage();
+    LocationPage thirdFragment = new LocationPage();
+    SettingsPage fourthFragment = new SettingsPage();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
+        if (itemID == R.id.home) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, firstFragment)
+                    .commit();
+            return true;
+        } else if (itemID == R.id.schedule) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, secondFragment)
+                    .commit();
+            return true;
+        } else if (itemID == R.id.location) {
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, thirdFragment)
+                    .commit();
+            return true;
+        } else if (itemID == R.id.settings) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, fourthFragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
