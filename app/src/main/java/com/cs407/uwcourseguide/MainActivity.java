1c7dd7bfed03e1d309edf358f5819d4edbecc09e
PrasunGuragain <guragain@wisc.edu>
2023-11-20 16:46:12 -0600

    Small change to main activity

--- app/src/main/java/com/cs407/uwcourseguide/MainActivity.java
+++ app/src/main/java/com/cs407/uwcourseguide/MainActivity.java
@@ -2,6 +2,8 @@ package com.cs407.uwcourseguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Main Activity will be the login page, unless user already logged. Similar to Notes lab
+        SharedPreferences sharedPreferences = getSharedPreferences("com.cs407.uwcourseguide", Context.MODE_PRIVATE);
+
+        if (sharedPreferences.getString("username", "") != "") {
+            // go to another screen, probably Home page
+        } else {
+            // stay in login page and add functionalities to the buttons
+        }
     }
 }
