96b7dc9e59556e7a7d27aa48d717f80bb28e0cd2
amohanty03 <mohanty5@wisc.edu>
2023-11-22 19:16:33 -0600

    Fixed navbar fragment functionality, commented out icon selected background color temporarily, buttons haven't been implemented yet

--- app/src/main/java/com/cs407/uwcourseguide/MainActivity.java
+++ app/src/main/java/com/cs407/uwcourseguide/MainActivity.java
@@ -2,11 +2,17 @@ package com.cs407.uwcourseguide;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowInsetsControllerCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,17 +21,27 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     BottomNavigationView bottomNavigationView;
 
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView
-                = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        //bottomNavigationView = findViewById(R.id.main_navigation);
 
         bottomNavigationView
                 .setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.home);
+
+        bottomNavigationView.setBackgroundColor(Color.parseColor("#FFFFFF"));
+
+        Drawable customBackground = getResources().getDrawable(R.drawable.custom_background);
+
+        // Set the item background programmatically
+        //bottomNavigationView.setItemBackground(customBackground);
+
         getSupportActionBar().hide();
     }
 
@@ -37,6 +53,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int itemID = item.getItemId();
+
         if (itemID == R.id.home) {
             getSupportFragmentManager()
                     .beginTransaction()
@@ -50,7 +67,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                     .commit();
             return true;
         } else if (itemID == R.id.location) {
-
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.flFragment, thirdFragment)
