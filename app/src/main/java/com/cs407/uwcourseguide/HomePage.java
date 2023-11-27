f015b4ff107b2c5c6b46138ec7359fe1aa374451
nealsidhu <nsidhu@wisc.edu>
2023-11-26 22:00:29 -0600

    work in progress

--- app/src/main/java/com/cs407/uwcourseguide/HomePage.java
+++ app/src/main/java/com/cs407/uwcourseguide/HomePage.java
@@ -1,17 +1,49 @@
 package com.cs407.uwcourseguide;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
+import androidx.fragment.app.FragmentManager;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class HomePage extends Fragment {
+public class HomePage extends AppCompatActivity //extends Fragment {
 
     public HomePage() {
         // Required empty public constructor
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Button firstFragmentButton = findViewById(R.id.firstFragmentButton);
+        firstFragmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, FirstFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("showing Courses")
+                        .commit();
+            }
+        });
+
+        Button secondFragmentButton = findViewById(R.id.secondFragmentButton);
+        secondFragmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, SecondFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("showing Professors")
+                        .commit();
+            }
+        });
     }
 
     @Override
